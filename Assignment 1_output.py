c41e03dd7a317ae0cc712eef782c9d8d83f9bd91
rahul-07048 <97098517+rahul-07048@users.noreply.github.com>
2022-01-06 20:19:12 +0530

    Add files via upload

---
+++ Assignment 1_output.py
@@ -0,0 +1,43 @@
+#output of 1st question
+
+
+Enter 1st number1
+Enter 2nd number2
+Enter 3rd number3
+Average:  2.0
+
+
+
+# Output of 2nd question
+
+enter gross amount in dollar($): 1000000
+enter the number of dependents: 34
+177600.0
+
+
+#Output of 3rd question
+
+18104107
+Rahul
+M
+ Intoduction to computing
+6.7
+[18104107, 'Rahul', 'M', 'Intoduction to computing', 6.7]
+
+
+
+#Output of 4th question
+
+
+[44, 55, 65, 75, 88]
+
+
+#Output of 5th question
+
+a.)
+
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+b.)
+
+['Red', 'Green', 'White', 'purple', 'Yellow']
