e3b86b71c1a4c82162cf84af4c535827643f4d89
rahul-07048 <97098517+rahul-07048@users.noreply.github.com>
2022-01-17 17:23:19 +0530

    Add files via upload

---
+++ Asg_2_Output.py
@@ -0,0 +1,73 @@
+output (result)
+
+ question 1
+
+a) Enter string: python is a case sensitive language
+35
+
+b)egaugnal evitisnes esac a si nohtyp
+
+c)a case sensitive
+
+d)python is object oriented language
+
+e)index of a in  python is a case sensitive language  is  10
+
+f)pythonisacasesensitivelanguage
+
+ question 2
+
+Hey, Rahul here!
+My SID is 18104107
+I am from Electrical department and my CGPA is 7.8
+
+question 3
+
+a) 8
+
+b) 58
+
+c) 50
+
+d) 224
+   40
+
+e) 14
+   0
+
+ question 4
+
+Enter 1st number: 4
+Enter 2nd number: 7
+Enter 3rd number: 12
+12 is greater than 4 and 7
+
+question 5
+
+
+Enter a string: hy i am rahul
+enter name: rahul
+Yes
+
+question 6
+
+1) 1st input
+
+Enter 1st length: 1.2
+Enter 2nd length: 4.5
+Enter 3rd length: 2.3
+
+
+No
+
+2) 2nd input
+
+Enter 1st length: 5.6
+Enter 2nd length: 7.5
+Enter 3rd length: 8.4
+
+
+Yes
+5
+7
+8
