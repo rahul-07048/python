afb10a06a81a6561d1bfb0994c63db97013abcdf
rahul-07048 <97098517+rahul-07048@users.noreply.github.com>
2022-02-19 11:51:46 +0530

    Add files via upload

---
+++ OUTPUT ASG_3.py
@@ -0,0 +1,85 @@
+OUTPUTS
+
+Q1
+
+enter name: rahul mahawar
+enter letter: a
+4
+
+
+Q2
+
+
+enter year: 2017
+enter month (1-12): 12
+enter day (1-31): 31
+Next date is [dd-mm-yyyy] 1-1-2018.
+
+
+Q3
+
+
+[(3, 9), (9, 81), (10, 100), (12, 144), (23, 529)]
+
+
+Q4
+
+
+Grade- 6
+Grade is C+ and Average Performance. 
+
+
+Q5
+
+enter rows: 11
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Q6
+
+Enter SID: 18104107
+Enter Name: Rahul mahawar
+Do you want to enter another student details(Y or N): Y
+Enter SID: 18104005
+Enter Name: sahil
+Do you want to enter another student details(Y or N): N
+a. {18104107: 'Rahul mahawar', 18104005: 'sahil'}
+b. {18104107: 'Rahul mahawar', 18104005: 'sahil'}
+c. {18104005: 'sahil', 18104107: 'Rahul mahawar'}
+Please Enter SID Of The Student You Want To Search: 18104107
+d. Student With The Given SID Is:  Rahul mahawar
+
+
+Q7
+
+Enter The Number Of Terms In The Seires: 12
+Resultant Fibonacci sequence: 
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+Average Of The Resultant Fibonacci Series =  19.333333333333332
+
+
+
+Q8
+
+
+
+a.  {1, 3, 5, 6, 8}
+b.  {3, 6, 8, 9, 13, 17}
+c.  {1, 2, 4, 5}
+d.  {6, 7, 8, 9, 10}
+e.  {10, 7}
