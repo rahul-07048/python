eeb2da1d9c867761228fe44d1068d204e32deff9
rahul-07048 <97098517+rahul-07048@users.noreply.github.com>
2022-03-03 11:33:29 +0530

    Add files via upload

---
+++ output_asg-4.py
@@ -0,0 +1,93 @@
+#q1
+
+-------------1--------------
+Transfer disk from A to C
+Transfer disk from A to B
+Transfer disk from C to B
+Transfer disk from A to C
+Transfer disk from B to A
+Transfer disk from B to C
+Transfer disk from A to C
+
+
+#q2
+
+-------------2(A)------------
+Using iteration-
+Give the no. of rows-5
+    1  
+
+   1  1  
+
+  1  2  1  
+
+ 1  3  3  1  
+
+1  4  6  4  1  
+
+--------------2(B)-------------
+Using recursion-
+Please give no. of rows-6
+            1 
+
+           1 1 
+
+          1 2 1 
+
+         1 3 3 1 
+
+        1 4 6 4 1 
+
+       1 5 10 10 5 1 
+
+Done!
+
+
+#q3
+
+-------------3(A)-----------
+Please give the first number-7
+Please give the second number-8
+The quotient is- 0
+The remainder is- 7
+[0, 7]
+Callable- True
+-------------3(B)-------------
+a is zero- False
+b is zero- False
+quotient is zero- True
+remainder is zero- False
+-------------3(C)-------------
+[0, 7, 4, 5, 6]
+The values greater than 4- [7, 5, 6]
+-------------3(D)-------------
+{5, 6, 7}
+-------------3(E)-------------
+The required immutable set frozenset({5, 6, 7})
+-------------3(F)-------------
+The required max value is- 7
+The required hash value is- 7
+
+#q4
+
+--------------4---------------
+Tony
+7
+
+#q5
+
+
+Destructor called,The object is destroyed.
+-------------5(A)---------
+The updated salary of Mehak is- 70000
+-------------5(B)---------
+The record of Viren has been successfully deleted-
+
+#q6
+
+-------------6-------------
+Please give a word-rahul
+Give a word-mahawar
+Possible Words- ['rahul', 'arhul', 'ahrul', 'ahurl', 'ahulr', 'rhaul', 'hraul', 'harul', 'haurl', 'haulr', 'rhual', 'hrual', 'hural', 'huarl', 'hualr', 'rhula', 'hrula', 'hurla', 'hulra', 'hular', 'rauhl', 'aruhl', 'aurhl', 'auhrl', 'auhlr', 'ruahl', 'urahl', 'uarhl', 'uahrl', 'uahlr', 'ruhal', 'urhal', 'uhral', 'uharl', 'uhalr', 'ruhla', 'urhla', 'uhrla', 'uhlra', 'uhlar', 'raulh', 'arulh', 'aurlh', 'aulrh', 'aulhr', 'rualh', 'uralh', 'uarlh', 'ualrh', 'ualhr', 'rulah', 'urlah', 'ulrah', 'ularh', 'ulahr', 'rulha', 'urlha', 'ulrha', 'ulhra', 'ulhar', 'rahlu', 'arhlu', 'ahrlu', 'ahlru', 'ahlur', 'rhalu', 'hralu', 'harlu', 'halru', 'halur', 'rhlau', 'hrlau', 'hlrau', 'hlaru', 'hlaur', 'rhlua', 'hrlua', 'hlrua', 'hlura', 'hluar', 'ralhu', 'arlhu', 'alrhu', 'alhru', 'alhur', 'rlahu', 'lrahu', 'larhu', 'lahru', 'lahur', 'rlhau', 'lrhau', 'lhrau', 'lharu', 'lhaur', 'rlhua', 'lrhua', 'lhrua', 'lhura', 'lhuar', 'raluh', 'arluh', 'alruh', 'alurh', 'aluhr', 'rlauh', 'lrauh', 'laruh', 'laurh', 'lauhr', 'rluah', 'lruah', 'lurah', 'luarh', 'luahr', 'rluha', 'lruha', 'lurha', 'luhra', 'luhar']
+If Barbie's word lies in the list,then their friendship is real.
+Friendship is fake.
